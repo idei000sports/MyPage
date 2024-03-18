8ecb4852202248d9404b6a443d864f964fd80d5e
idei000sports <idei000sports@gmail.com>
2024-03-18 15:29:59 +0900

    make drawer menu

--- app/header.js
+++ app/header.js
@@ -5,17 +5,41 @@ export default function Header() {
         <header className="fixed-top">
             <div className="container">
                 <div className="row">
-                    <div className="col text-start text-white">
-                        <p>...</p>
+                    <div className="col"></div>
+                    <div className="col text-center">
+                        <h1 className="text-white ">ASSHOLE</h1>
                     </div>
-                    <div className="col text-center text-white">
-                        <h1 className="aaa">ASSHOLE</h1>
-                    </div>
-                    <div class="col text-end text-white">
-                        <p className="aaa">記号</p>
+                    <div className="col text-end">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                        <i class="h1 text-white bi bi-list"></i>
+                    </button>
                     </div>
                 </div>
+                
             </div>
-        </header>
+
+            <nav className="navbar fixed-top">
+                <div className="container">
+                    <a className="navbar-brand" href="#"></a>
+                    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">ASSHOLE</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul>
+                                <li>けつ</li>
+                                <li>けつ</li>
+                                <li>けつ</li>
+                                <li>けつ</li>
+                                <li>けつ</li>
+                                <li>けつ</li>
+                                <li>けつ</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header >
     );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import Header from "./header"
 
 
@@ -17,6 +18,8 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
           {children}
+          <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
       </body>
     </html>
   );
