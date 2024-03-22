717dd6e59617ebe1c5fc8c03a3930a68e68de003
idei000sports <idei000sports@gmail.com>
2024-03-22 17:04:19 +0900

    データベース替え

--- prisma/migrations/20240321022217_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "City" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "country" TEXT NOT NULL,
-    "state" TEXT NOT NULL,
-    "city" TEXT NOT NULL
-);
---
+++ prisma/migrations/20240322075859_/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "notes" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "notes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Origin" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "county" TEXT NOT NULL,
+    "formation" INTEGER NOT NULL,
+    "dissolution" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Origin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "City" (
+    "id" SERIAL NOT NULL,
+    "country" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "county" TEXT NOT NULL,
+    "lattitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "City_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Origin_name_county_key" ON "Origin"("name", "county");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "City_country_state_county_key" ON "City"("country", "state", "county");
