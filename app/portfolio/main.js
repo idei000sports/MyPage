19b51a22dcd1a02007f3888bc6453ccad6286fde
idei000sports <idei000sports@gmail.com>
2024-05-29 17:07:49 +0900

    デザイン調整

--- app/portfolio/main.js
+++ app/portfolio/main.js
@@ -6,9 +6,13 @@ export default function Main() {
             <main>
                 <div className="profile">
                     <div className="profile_text">
-                        <h5 className="profile_h5">肩書</h5>
+                        <h5 className="profile_h5">チャンピオンシップロードランナー</h5>
                         <h2>出射 拓馬</h2>
                         <h5 className="profile_h5">Idei Takuma</h5>
+                        <p>
+                            1992-
+                        </p>
+                        <p>大阪府</p>
                     </div>
                     <div className="profile_image">
                         <Image src="/top/me_ver2.jpg" layout="fill" objectFit="cover" />
@@ -16,7 +20,8 @@ export default function Main() {
                 </div>
 
                 <div>
-                    <h2 className="title">main</h2>
+                    <h2 className="title"><span className="content">Contents</span></h2>
+
                     <dl>
                         <dt><a href="/dajare">ダジャレ作成機</a></dt>
                         <dd>単語を入力すると母音が一致する単語をリストアップする</dd>
@@ -27,13 +32,12 @@ export default function Main() {
                         <dt><a href="/home/infomation">お問い合わせ</a></dt>
                         <dd>連絡先未入力</dd>
                     </dl>
-
                 </div>
 
                 <div>
-                    <h2 className="title">content2</h2>
+                    <h2 className="title"><span className="content">Contents</span></h2>
                     <ul>
-                        <li className="content_list">aaaa</li>
+                        <li className="content_list">7600000000年XX月XX日 地球崩壊</li>
                         <li className="content_list">aaaa</li>
                         <li className="content_list">aaaa</li>
                     </ul>
